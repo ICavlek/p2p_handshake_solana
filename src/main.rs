0ac4088d986cfec71a7464bbcf4b019c751fba4b
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-17 01:00:25 +0100

    Init established connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Client;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    let http_client = Client::builder()
+        .timeout(Duration::from_millis(200))
+        .build()
+        .unwrap();
+
+    let response = http_client
+        .post("http://127.0.0.1:8899")
+        .header("Content-Type", "application/json")
+        .body(r#"{"jsonrpc":"2.0","id":1,"method":"getHealth"}"#)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    println!("{:#?}", response);
 }
