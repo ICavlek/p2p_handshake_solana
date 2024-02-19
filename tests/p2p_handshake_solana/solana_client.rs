81c4962f066e91bb3f69d12412dafbd315fe75b6
ICavlek <ivan.cavlek1403@gmail.com>
2024-02-19 20:46:19 +0100

    Implemented tests for different HTTP responses

--- tests/p2p_handshake_solana/solana_client.rs
+++ tests/p2p_handshake_solana/solana_client.rs
@@ -1,4 +1,4 @@
-use p2p_handshake_solana::solana_client::SolanaClient;
+use p2p_handshake_solana::solana_client::{SolanaClient, SolanaClientError};
 use wiremock::{matchers::method, Mock, MockServer, ResponseTemplate};
 
 #[tokio::test]
@@ -15,7 +15,7 @@ async fn handshake_returns_200_for_valid_form_data() {
 }
 
 #[tokio::test]
-async fn remote_node_returns_500() {
+async fn remote_node_returns_500_for_internal_server_error() {
     let mock_server = MockServer::start().await;
     let solana_client = SolanaClient::new(mock_server.uri());
 
@@ -23,11 +23,27 @@ async fn remote_node_returns_500() {
         .respond_with(ResponseTemplate::new(500))
         .mount(&mock_server)
         .await;
-    let response = solana_client.get_version().await.unwrap();
-    assert_eq!(response.status().as_u16(), 500);
+    let response = solana_client.handshake().await;
+    assert!(matches!(
+        response,
+        Err(SolanaClientError::HttpResponseError)
+    ));
 }
 
-// TODO Connection time out test
-// TODO Connection failed test
+#[tokio::test]
+async fn remote_node_returns_408_for_connection_timeout_error() {
+    let mock_server = MockServer::start().await;
+    let solana_client = SolanaClient::new(mock_server.uri());
+
+    Mock::given(method("POST"))
+        .respond_with(ResponseTemplate::new(408))
+        .mount(&mock_server)
+        .await;
+    let response = solana_client.handshake().await;
+    assert!(matches!(
+        response,
+        Err(SolanaClientError::HttpResponseError)
+    ));
+}
 // TODO Wrong data test
 // TODO Correct data test
